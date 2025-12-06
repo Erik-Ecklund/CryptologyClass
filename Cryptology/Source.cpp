3098364fdc55d35df13778456b9dad0b1d426c42
Erik-Ecklund <168017305+Erik-Ecklund@users.noreply.github.com>
2025-12-06 11:03:58 -0500

    idk

--- Cryptology/Source.cpp
+++ Cryptology/Source.cpp
@@ -8,9 +8,36 @@
 #include <iostream>
 #include <bitset>
 #include <string>
+#include <random>
+#include <chrono>
 using namespace std;
 
 int main() {
+	ullong64 secretkey = 1ULL << 20;
+	// if secretkey includes parity bits, increment them.
+	while (secretkey & paritybits) secretkey += secretkey & paritybits;
+	cout << "0x" << hex << uppercase << setw(16) << right << setfill('0') << secretkey << endl;
+	ullong64 plaintext = 0;
+	ullong64 vplaintext = 255;
+	ullong64 ciphertext = DESEncrypt(plaintext, bitset<64>(secretkey));
+	ullong64 verification = DESEncrypt(vplaintext, bitset<64>(secretkey));
+	ullong64 foundkey = 0;
+	auto start = chrono::high_resolution_clock::now();
+	while (true) {
+		// skip keys with parity bits set to reduce search space
+		while(foundkey & paritybits) foundkey += foundkey & paritybits;
+		ullong64 encryptedtext = DESEncrypt(plaintext, bitset<64>(foundkey));
+		if (encryptedtext == ciphertext) break;
+		foundkey++;
+	}
+	auto end = chrono::high_resolution_clock::now();
+	cout << "Found Key: 0x" << hex << uppercase << setw(16) << right << setfill('0') << foundkey;
+	cout << "Time Taken: " << chrono::duration<double>(end - start).count() << " seconds.";
+	return 0;
+}
+
+
+/*
 	string keyString = "";
 	cout << "Enter Key? [y/n] ";
 	char answer;

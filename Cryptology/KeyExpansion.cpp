2c50430184a70d206ac1d4709ca714d2761f844a
Erik-Ecklund <168017305+Erik-Ecklund@users.noreply.github.com>
2025-12-06 11:46:11 -0500

    move string keyexpansion functions to bottom of file

--- Cryptology/KeyExpansion.cpp
+++ Cryptology/KeyExpansion.cpp
@@ -1,21 +1,5 @@
 #include "KeyExpansion.h"
 
-void KeyExpansion(k64 key, bool doHex) {
-	b56 c0d0 = PermutedChoice1(key);
-	b28 c = c0d0.substr(0, 28);
-	b28 d = c0d0.substr(28, 28);
-	for (int round = 0; round < 16; round++) {
-		c = Shift(c, round);
-		d = Shift(d, round);
-		b56 cd = c + d;
-		string subkey = PermutedChoice2(cd);
-		if (doHex) {
-			subkey = BinToHex(subkey);
-		}
-		cout << "K" << setw(2) << left << round + 1 << " = " << subkey << endl;
-	}
-}
-
 void KeyExpansion(ullong48 subkeys[16], bitset<64> key, bool printHex, bool printBin) {
 	ullong56 cd = PermutedChoice1(key.to_ullong());
 	for (int round = 0; round < 16; round++) {
@@ -31,22 +15,6 @@ void KeyExpansion(ullong48 subkeys[16], bitset<64> key, bool printHex, bool prin
 	}
 }
 
-b56 PermutedChoice1(k64 key) {
-	static constexpr int PC1[] = { 
-		57, 49, 41, 33, 25, 17, 9, 
-		1, 58, 50, 42, 34, 26, 18, 
-		10, 2, 59, 51, 43, 35, 27, 
-		19, 11, 3, 60, 52, 44, 36, 
-		63, 55, 47, 39, 31, 23, 15, 
-		7, 62, 54, 46, 38, 30, 22, 
-		14, 6, 61, 53, 45, 37, 29, 
-		21, 13, 5, 28, 20, 12, 4 
-	};
-	b56 output_c0d0 = "";
-	for (int i = 0; i < size(PC1); i++)
-		output_c0d0 += key[PC1[i] - 1];
-	return output_c0d0;
-}
 
 ullong56 PermutedChoice1(ullong64 key) {
 	static constexpr int PC1[] = { 
@@ -66,20 +34,6 @@ ullong56 PermutedChoice1(ullong64 key) {
 	return output_c0d0;
 }
 
-k48 PermutedChoice2(b56 cd) {
-	static constexpr int PC2[] = { 
-		14, 17, 11, 24, 1, 5, 3, 28, 
-		15, 6, 21, 10, 23, 19, 12, 4, 
-		26, 8, 16, 7, 27, 20, 13, 2, 
-		41, 52, 31, 37, 47, 55, 30, 40, 
-		51, 45, 33, 48, 44, 49, 39, 56, 
-		34, 53, 46, 42, 50, 36, 29, 32 
-	};
-	k48 subkey = "";
-	for (int i = 0; i < size(PC2); i++)
-		subkey += cd[PC2[i] - 1];
-	return subkey;
-}
 
 ullong48 PermutedChoice2(ullong56 cd) {
 	static constexpr int PC2[] = { 
@@ -97,17 +51,6 @@ ullong48 PermutedChoice2(ullong56 cd) {
 	return subkey;
 }
 
-
-b28 Shift(b28 bin, int round) {
-	static constexpr int ShiftSched[] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
-	bin += bin[0];
-	if (ShiftSched[round] == 2) {
-		bin += bin[1];
-	}
-	bin.erase(0, ShiftSched[round]);
-	return bin;
-}
-
 ullong56 Shift(ullong56 cd, int round) {
 	static constexpr int ShiftSched[] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
 	ullong28 c = (cd >> 28) & 0xFFFFFFF;
@@ -120,6 +63,66 @@ ullong56 Shift(ullong56 cd, int round) {
 
 ullong28 RotateLeft(ullong28 input, int shift) {
 	return ((input << shift) | (input >> (28 - shift))) & 0xFFFFFFF;
+}
+
+
+// string functions
+void KeyExpansion(k64 key, bool doHex) {
+	b56 c0d0 = PermutedChoice1(key);
+	b28 c = c0d0.substr(0, 28);
+	b28 d = c0d0.substr(28, 28);
+	for (int round = 0; round < 16; round++) {
+		c = Shift(c, round);
+		d = Shift(d, round);
+		b56 cd = c + d;
+		string subkey = PermutedChoice2(cd);
+		if (doHex) {
+			subkey = BinToHex(subkey);
+		}
+		cout << "K" << setw(2) << left << round + 1 << " = " << subkey << endl;
+	}
+}
+
+b56 PermutedChoice1(k64 key) {
+	static constexpr int PC1[] = {
+		57, 49, 41, 33, 25, 17, 9,
+		1, 58, 50, 42, 34, 26, 18,
+		10, 2, 59, 51, 43, 35, 27,
+		19, 11, 3, 60, 52, 44, 36,
+		63, 55, 47, 39, 31, 23, 15,
+		7, 62, 54, 46, 38, 30, 22,
+		14, 6, 61, 53, 45, 37, 29,
+		21, 13, 5, 28, 20, 12, 4
+	};
+	b56 output_c0d0 = "";
+	for (int i = 0; i < size(PC1); i++)
+		output_c0d0 += key[PC1[i] - 1];
+	return output_c0d0;
+}
+
+k48 PermutedChoice2(b56 cd) {
+	static constexpr int PC2[] = {
+		14, 17, 11, 24, 1, 5, 3, 28,
+		15, 6, 21, 10, 23, 19, 12, 4,
+		26, 8, 16, 7, 27, 20, 13, 2,
+		41, 52, 31, 37, 47, 55, 30, 40,
+		51, 45, 33, 48, 44, 49, 39, 56,
+		34, 53, 46, 42, 50, 36, 29, 32
+	};
+	k48 subkey = "";
+	for (int i = 0; i < size(PC2); i++)
+		subkey += cd[PC2[i] - 1];
+	return subkey;
+}
+
+b28 Shift(b28 bin, int round) {
+	static constexpr int ShiftSched[] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+	bin += bin[0];
+	if (ShiftSched[round] == 2) {
+		bin += bin[1];
+	}
+	bin.erase(0, ShiftSched[round]);
+	return bin;
 }
 
 h48 BinToHex(k48 subkey) {
